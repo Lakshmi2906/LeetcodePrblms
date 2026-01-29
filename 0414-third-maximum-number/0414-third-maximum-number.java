373ae85beb86e0b3ed0f3974280ffdac041d843b
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-01-29 18:53:08 +0530

    Time: 3 ms (75.32%), Space: 45 MB (58.38%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,18 +1,28 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        TreeSet<Integer> ts = new TreeSet<>();
+        Long first = null, second = null, third = null;
 
         for (int num : nums) {
-            ts.add(num);
+            long n = num;
+
+            if ((first != null && n == first) ||
+                (second != null && n == second) ||
+                (third != null && n == third)) {
+                continue;
+            }
+
+            if (first == null || n > first) {
+                third = second;
+                second = first;
+                first = n;
+            } else if (second == null || n > second) {
+                third = second;
+                second = n;
+            } else if (third == null || n > third) {
+                third = n;
+            }
         }
 
-        if (ts.size() < 3) {
-            return ts.last();
-        }
-
-        ts.pollLast();
-        ts.pollLast();
-
-        return ts.last();
+        return third == null ? first.intValue() : third.intValue();
     }
 }
