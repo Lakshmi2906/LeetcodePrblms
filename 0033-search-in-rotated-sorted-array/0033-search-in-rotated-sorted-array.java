e1b610633d60cef48da96e336e72f796fa1800ed
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-04-09 20:04:13 +0530

    Time: 0 ms (100%), Space: 43.7 MB (85.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,10 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        int low=0,high=nums.length-1;
+        int min=minValue(nums);
+        int ans=binarySearch(nums,0,min-1,target);
+        if(ans==-1){
+            ans=binarySearch(nums,min,nums.length-1,target);
+        }
+        return ans;
+    }
+    public int binarySearch(int arr[],int left,int right,int target){
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
             }
         }
         return -1;
+    }
+    public int minValue(int[] nums){
+        int low=0,high=nums.length-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(nums[mid]<nums[high]){
+                high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 }
