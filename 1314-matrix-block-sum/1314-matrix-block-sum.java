adde0a658d03bba685d41c11da3e3976c1966ee8
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-05-28 17:35:59 +0530

    Time: 6 ms (94.22%), Space: 46.9 MB (22.57%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int row=mat.length;
-        int col=mat[0].length;
-        int ans[][] = new int[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                int sum=0;
-                for(int r=i-k;r<=i+k;r++){
-                    for(int c=j-k;c<=j+k;c++){
-                        if(r>=0 && r<row && c>=0 && c<col){
-                          sum+=mat[r][c];
-                        }
-                    }
-                }
-                ans[i][j]=sum;
+        int m = mat.length, n = mat[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i+1][j+1] = mat[i][j] 
+                             + dp[i][j+1] 
+                             + dp[i+1][j] 
+                             - dp[i][j];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int r1 = Math.max(0, i-k), c1 = Math.max(0, j-k);
+                int r2 = Math.min(m-1, i+k), c2 = Math.min(n-1, j+k);
+
+                ans[i][j] = dp[r2+1][c2+1] 
+                          - dp[r1][c2+1] 
+                          - dp[r2+1][c1] 
+                          + dp[r1][c1];
             }
         }
         return ans;
