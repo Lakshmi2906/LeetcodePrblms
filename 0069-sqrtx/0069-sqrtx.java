a402d5b326b65e15609ecc5c5b5bda7663baa8b5
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-06-11 21:47:39 +0530

    Time: 1 ms (98.04%), Space: 42.6 MB (60.24%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        long r=x;
-        while(r*r>x){
-            r=(r+(x/r))/2;
+        long left=0,right=x-1;
+        if(x<2) return x;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            long sqrt=mid*mid;
+            if(sqrt==x) return (int) mid;
+            else if(sqrt<x) left=mid+1;
+            else right=mid-1;
         }
-        return (int) r;
+        return (int) right;
     }
 }
