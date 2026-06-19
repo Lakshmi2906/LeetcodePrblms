a65feed3e5c521bde972f4626798fde97ec48acd
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-06-19 22:00:30 +0530

    Time: 0 ms (100%), Space: 43.8 MB (64.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0,high=nums.length-1;
-        int min=minValue(nums);
-        int ans=binarySearch(nums,0,min-1,target);
-        if(ans==-1){
-            ans=binarySearch(nums,min,nums.length-1,target);
-        }
-        return ans;
-    }
-    public int binarySearch(int arr[],int left,int right,int target){
+        int left=0,right=nums.length-1;
         while(left<=right){
-            int mid=(left+right)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                left=mid+1;
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
             }
             else{
-                right=mid-1;
+                if(target<=nums[right] && nums[mid]<target){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
             }
         }
         return -1;
-    }
-    public int minValue(int[] nums){
-        int low=0,high=nums.length-1;
-        while(low<high){
-            int mid=(low+high)/2;
-            if(nums[mid]<nums[high]){
-                high=mid;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return low;
     }
 }
