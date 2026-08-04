b897245d1098773ffff480c20181d259ec265031
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-08-04 21:20:43 +0530

    Time: 2 ms (92.13%), Space: 47.6 MB (88.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] ans=new int[nums.length];
         int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                ans[k]=nums[i];
+                nums[k]=nums[i];
                 k++;
             }
         }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=ans[i];
+        while(k<nums.length){
+            nums[k]=0;
+            k++;
         }
     }
 }
