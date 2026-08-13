da6696ce4b6a99c81c66f2f33448acbb71b05a5b
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-08-13 22:10:11 +0530

    Time: 15 ms (61.42%), Space: 48.2 MB (76.14%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-       Stack<Integer> st=new Stack<>();
-       int n=nums.length;
-       int[] ans=new int[n];
-       for(int i=2*n-1;i>=0;i--){
+        int n=nums.length;
+        int ans[]=new int[n];
+        Arrays.fill(ans,-1);
+        Stack<Integer> st=new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
             int num=nums[i%n];
-            while(!st.empty() && st.peek()<=num){
+            while(!st.isEmpty() && st.peek()<=num){
                 st.pop();
             }
-            if(i<n){
-                ans[i]=st.empty()?-1:st.peek();
+            if(i<n && !st.isEmpty()){
+                 ans[i%n]=st.peek();
             }
             st.push(num);
-       }
-       return ans;
+        }
+        return ans;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
