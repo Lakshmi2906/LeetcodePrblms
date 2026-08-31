04c0a4abbb4084f3cbed7817f37918683d1e408d
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-08-31 21:46:43 +0530

    Time: 39 ms (37.93%), Space: 47.1 MB (59.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int search=target-nums[i];
-            if(hm.containsKey(search)){
-                return new int[] {hm.get(search),i};
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
             }
-            hm.put(nums[i],i);
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
