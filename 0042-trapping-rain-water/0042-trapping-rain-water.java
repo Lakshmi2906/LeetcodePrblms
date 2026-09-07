52736c628fbcd8486bb8930a86f425686ed7d9ac
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-09-07 17:58:25 +0530

    Time: 1 ms (57.07%), Space: 47.2 MB (98.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,16 +2,16 @@ class Solution {
     public int trap(int[] height) {
         int tw=0,lmax=0,rmax=0,left=0,right=height.length-1;
         while(left<right){
-            lmax=Math.max(lmax,height[left]);
-            rmax=Math.max(rmax,height[right]);
-           if(height[left]<height[right]){
-            tw+=lmax-height[left];
-            left++;
-           }
-           else{
-            tw+=rmax-height[right];
-            right--;
-           }
+            lmax=Math.max(height[left],lmax);
+            rmax=Math.max(height[right],rmax);
+            if(height[left]<height[right]){
+                tw+= lmax-height[left];
+                left++;
+            }
+            else{
+                tw+=rmax-height[right];
+                right--;
+            }
         }
         return tw;
     }
