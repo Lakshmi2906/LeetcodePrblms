75d9acf89b59c78c2763a6d019cad84c0a30cd43
Lakshmi Saravanan <147672520+Lakshmi2906@users.noreply.github.com>
2026-09-17 05:37:53 +0530

    Time: 3 ms (95.86%), Space: 52.2 MB (35.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,14 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-      int maxw=0;
-      for(int i=0;i<nums.length;i++){
-        int cntz=0;
-        for(int j=i;j<nums.length;j++){
-            if(nums[j]==0) cntz++;
-            if(cntz>k) break;
-             maxw=Math.max(maxw,j-i+1);
+        int l=0,maxcnt=0,cnt=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]==0){
+                cnt++;
+            }
+            while(cnt>k){
+                if(nums[l]==0) cnt--;
+                l++;
+            }
+            maxcnt=Math.max(maxcnt,r-l+1);
         }
-      } 
-      return maxw; 
+        return maxcnt;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
